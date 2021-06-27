fbada6ab3f090d237a70a1174e9330ca64908955
AristodamusAdairs <83016476+AristodamusAdairs@users.noreply.github.com>
2021-06-27 10:29:49 +0500

    Update helpers.hpp

--- src/Parser/source/helpers.hpp
+++ src/Parser/source/helpers.hpp
@@ -1,7 +1,7 @@
 #ifndef helpers_h
 #define helpers_h
 
-namespace stork {
+namespace ubiquit {
 
 	//blatantly stolen from https://en.cppreference.com/w/cpp/utility/variant/visit
 	template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
